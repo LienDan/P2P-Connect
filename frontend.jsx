b6e1b17beaa9d8cac5b492c608007b9958f12e38
Daniel <liend@uwindsor.ca>
2026-08-27 23:54:29 -0400

    Coded frontend.jsx to add dynamic React webpage content, and implemented form. Layout and design of frontend is fairly simple so I will need to make it look nicer in a future update. Also, will have to figure out how to connect to backend using Electron. Still a lot of work left to do.

--- frontend.jsx
+++ frontend.jsx
@@ -1,11 +1,49 @@
 import { createRoot } from 'react-dom/client';
 console.log("This is a test setup from the react tutorial page.");
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+function tryConnect(submission) {
+  submission.preventDefault();
+  console.log(submission);
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+  // Read the form data
+  const form = submission.target;
+  const formData = new FormData(form);
+  console.log("Your IP: " + formData.get("yourIP") + "\nPeer IP: " + formData.get("peerIP") + "\nYour Port: " + formData.get("yourPort") + "\nPeer Port: " + formData.get("peerPort"));
 
-console.log("Test Print");
+}
+
+function ConnectionForm(){
+    //reminder that react component needs to start with a capital, so the function name needs to start with captial
+    return (
+    <form id="setup" onSubmit={tryConnect}>
+      <div className="inputs">
+        <label htmlFor="yourIP">Your IP </label>
+        <input type="text" id="yourIP" name="yourIP" required />
+      </div>
+
+      <div className="inputs">
+        <label htmlFor="peerIP">Peer IP </label>
+        <input type="text" id="peerIP" name="peerIP" required />
+      </div>
+
+      <div className="inputs">
+        <label htmlFor="yourPort">Your Port </label>
+        <input type="number" id="yourPort" name="yourPort" min="0" max="65535" required />
+      </div>
+
+      <div className="inputs">
+        <label htmlFor="peerPort">Peer Port</label>
+        <input type="number" id="peerPort" name="peerPort" min="0" max="65535" required
+        />
+      </div>
+
+      <div className="inputs">
+        <button type="submit" id="connectButton">Connect</button>
+      </div>
+    </form>
+  );
+}
+
+
+let root = createRoot(document.getElementById('reactApp'));
+root.render(<ConnectionForm />);
